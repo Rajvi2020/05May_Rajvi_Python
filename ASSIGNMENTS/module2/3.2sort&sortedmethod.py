7d98d776e12f07a1159d80e2a55e5712a8bd133e
Rajvi2020 <rajvi.gandhi2022@gmail.com>
2026-05-29 10:50:57 +0530

    dict

---
+++ ASSIGNMENTS/module2/3.2sort&sortedmethod.py
@@ -0,0 +1,9 @@
+# Original list
+numbers = [5, 2, 9, 1, 7]
+# Using sort() method
+numbers.sort()
+print("List after sort():", numbers)
+numbers2 = [8, 3, 6, 4, 1]
+sorted_numbers = sorted(numbers2)
+print("Original list:", numbers2)
+print("List after sorted():", sorted_numbers)
