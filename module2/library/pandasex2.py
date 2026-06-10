156ac22a5ee128e316a7240af319f060f7786965
Rajvi2020 <rajvi.gandhi2022@gmail.com>
2026-06-10 11:37:21 +0530

    =

---
+++ module2/library/pandasex2.py
@@ -0,0 +1,4 @@
+import instaloader
+instaid='topstech'
+insta=instaloader.Instaloader()
+insta.download_profile(instaid)
---
+++ module2/library/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+import pandas
+stdata={
+  'id':[1,2,4,5,7]
+}
+dt=pandas.DataFrame(stdata)
+print(dt)
