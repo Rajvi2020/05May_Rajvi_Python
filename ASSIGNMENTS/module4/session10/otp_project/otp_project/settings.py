2ab72162c81548227e916c05f7313c2d8def537b
Rajvi2020 <rajvi.gandhi2022@gmail.com>
2026-08-04 21:08:47 +0530

    modified sesion10

--- ASSIGNMENTS/module4/session10/otp_project/otp_project/settings.py
+++ ASSIGNMENTS/module4/session10/otp_project/otp_project/settings.py
@@ -127,7 +127,7 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "rajvigandhi05@gmail.com"
+EMAIL_HOST_USER = ""
 
-EMAIL_HOST_PASSWORD = "iuvp viuk fucr sadd"
+EMAIL_HOST_PASSWORD = ""
 
